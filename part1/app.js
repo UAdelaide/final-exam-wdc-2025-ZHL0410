23e24b6c46cc33a3ab866abf4ee0899dd91cf1e6
ZHL0410 <a1920989@adelaide.edu.au>
2025-06-20 06:45:39 +0000

    [automatic commit] 2025-06-20 06:45:39 UTC 175.35.84.12

--- part1/app.js
+++ part1/app.js
@@ -162,7 +162,7 @@ app.get('/api/walkers/summary', async (req, res) => {
     const [rowsOfWalkerSummary] = await db.execute(`
         SELECT u.username AS walker_username
         COUNT(wra.rating_id) AS total_ratings
-        ROUND(AVE(wr.rating)
+        ROUND(AVE(wr.rating), 1) AS average
         FROM Users u
         WHERE u.role = 'walker'
         GROUP BY u.user_id
