f006903dc9f2d74df15f1768d3afd09c17ccbe46
ZHL0410 <a1920989@adelaide.edu.au>
2025-06-20 05:52:39 +0000

    [automatic commit] 2025-06-20 05:52:39 UTC 175.35.84.12

--- part1/app.js
+++ part1/app.js
@@ -108,7 +108,7 @@ app.get('/api/walkrequests/open', async (req, res) => {
     const [rowsOfWalkRequests] = await db.execute(`
         SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username AS owner_username
         FROM WalkRequests wr
-        JOIN 
+        JOIN Dogs d ON wr.
         JOIN Users u ON d.owner_id = u.user_id
         `);
     res.json(rowsOfWalkRequests);
