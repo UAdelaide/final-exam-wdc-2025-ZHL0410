bcf5346aed261092c81a535fc44e7109dca0540d
ZHL0410 <a1920989@adelaide.edu.au>
2025-06-20 23:05:36 +0000

    [automatic commit] 2025-06-20 23:05:36 UTC 175.35.84.12

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -90,13 +90,12 @@ router.get('/dogs', async (req, res) => {
   const ownerId = req.session.user.user_id;
 
  try {
-    const [rows] = await db.query('SELECT user_id, username, email, role FROM Users');
+    const [rows] = await db.query(`SELECT dog_id, name FROM Dogs WHERE owner_id = ?`, [ownerId]);
     res.json(rows);
   } catch (error) {
     res.status(500).json({ error: 'Failed to fetch users' });
   }
-  const sql = `SELECT dog_id, name FROM Dogs WHERE owner_id = ?`;
-  const [] = await db.query(sql, [ownerId]);
+
 });
 
 module.exports = router;
