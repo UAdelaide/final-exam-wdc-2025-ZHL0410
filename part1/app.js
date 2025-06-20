3c9a06a64ca417b33269af6cbc479006fb5d435c
ZHL0410 <a1920989@adelaide.edu.au>
2025-06-20 05:49:56 +0000

    [automatic commit] 2025-06-20 05:49:56 UTC 175.35.84.12

--- part1/app.js
+++ part1/app.js
@@ -106,11 +106,11 @@ app.get('/api/dogs', async (req, res) => {
 app.get('/api/walkrequests/open', async (req, res) => {
     try {
     const [rowsOfWalkRequests] = await db.execute(`
-        SELECT d.name AS dog_name, d.size, u.username
+        SELECT 
         FROM Dogs d
         JOIN Users u ON d.owner_id = u.user_id
         `);
-    res.json(rowsOf);
+    res.json(rowsOfWalkRequests);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch walkrequests' });
   }
