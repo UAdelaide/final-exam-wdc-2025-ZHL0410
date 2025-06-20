89847816bd60e9c1b722d6b62692053d4891d260
ZHL0410 <a1920989@adelaide.edu.au>
2025-06-20 06:55:14 +0000

    [automatic commit] 2025-06-20 06:55:14 UTC 175.35.84.12

--- part1/app.js
+++ part1/app.js
@@ -170,10 +170,10 @@ app.get('/api/walkers/summary', async (req, res) => {
           JOIN WalkApplications wa ON wa.request_id = wreq.request_id
           WHERE wa.walker_id = u.user_id AND wreq.status = 'completed' AND wa.status = 'accepted'
         ) AS completed_walks
-      FROM Users u
-      LEFT JOIN WalkRatings wr ON u.user_id = wr.walker_id
-      WHERE u.role = 'walker'
-      GROUP BY u.user_id
+        FROM Users u
+        LEFT JOIN WalkRatings wr ON u.user_id = wr.walker_id
+        WHERE u.role = 'walker'
+        GROUP BY u.user_id
         `);
     res.json(rowsOfWalkerSummary);
   } catch (err) {
