21902ed70d007c3d2348f70bcbd9e22acb4cb279
ZHL0410 <a1920989@adelaide.edu.au>
2025-06-20 06:38:08 +0000

    [automatic commit] 2025-06-20 06:38:08 UTC 175.35.84.12

--- part1/app.js
+++ part1/app.js
@@ -103,8 +103,8 @@ let db;
         await db.execute(`
             INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments)
             VALUES
-            (?, ?, ?, 5, ')
-            `);
+            (?, ?, ?, 5, 'Good job and my dog enjoys!');
+            `, []);
         await db.execute(`
         INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments)
         VALUES
