3771b74fa34bed27ab244d9f4f076867b2b43beb
ZHL0410 <a1920989@adelaide.edu.au>
2025-06-20 05:50:57 +0000

    [automatic commit] 2025-06-20 05:50:57 UTC 175.35.84.12

--- part1/app.js
+++ part1/app.js
@@ -106,7 +106,7 @@ app.get('/api/dogs', async (req, res) => {
 app.get('/api/walkrequests/open', async (req, res) => {
     try {
     const [rowsOfWalkRequests] = await db.execute(`
-        SELECT r
+        SELECT wr.request
         FROM Dogs d
         JOIN Users u ON d.owner_id = u.user_id
         `);
