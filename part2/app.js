2270b57de665e47c6f9b1a33d35ba04de00ed4f2
ZHL0410 <a1920989@adelaide.edu.au>
2025-06-20 12:13:36 +0000

    [automatic commit] 2025-06-20 12:13:36 UTC 175.35.84.12

--- part2/app.js
+++ part2/app.js
@@ -11,7 +11,9 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
 // Session
-app.use();
+app.use(session({
+    secret
+}));
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
