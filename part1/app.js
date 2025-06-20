0bb5550a19878b75066f78dc66dfb62bca9c6c79
ZHL0410 <a1920989@adelaide.edu.au>
2025-06-20 05:54:22 +0000

    [automatic commit] 2025-06-20 05:54:21 UTC 175.35.84.12

--- part1/app.js
+++ part1/app.js
@@ -110,7 +110,7 @@ app.get('/api/walkrequests/open', async (req, res) => {
         FROM WalkRequests wr
         JOIN Dogs d ON wr.dog_id = d.dog_id
         JOIN Users u ON d.owner_id = u.user_id
-        WHERE wr.status = open
+        WHERE wr.status = 'open'
         `);
     res.json(rowsOfWalkRequests);
   } catch (err) {
