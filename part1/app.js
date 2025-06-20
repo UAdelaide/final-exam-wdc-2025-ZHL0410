7c9a07ae5c9e8c616bb8e3a975fc06988964d0b0
ZHL0410 <a1920989@adelaide.edu.au>
2025-06-20 06:42:56 +0000

    [automatic commit] 2025-06-20 06:42:56 UTC 175.35.84.12

--- part1/app.js
+++ part1/app.js
@@ -159,14 +159,14 @@ app.get('/api/walkrequests/open', async (req, res) => {
 // Route to return a summary of each walker as JSON
 app.get('/api/walkers/summary', async (req, res) => {
     try {
-    const [rowsOfWalkRequests] = await db.execute(`
+    const [rowsOfWalkerSummary] = await db.execute(`
         SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username AS owner_username
         FROM WalkRequests wr
         JOIN Dogs d ON wr.dog_id = d.dog_id
         JOIN Users u ON d.owner_id = u.user_id
         WHERE wr.status = 'open'
         `);
-    res.json(rowsOfWalkRequests);
+    res.json(rowsOfWalkerSummary);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch walkrequests' });
   }
