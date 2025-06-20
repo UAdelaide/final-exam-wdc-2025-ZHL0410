b676a81436e265f28ed41cbfaf0048932abab729
ZHL0410 <a1920989@adelaide.edu.au>
2025-06-20 05:38:36 +0000

    [automatic commit] 2025-06-20 05:38:36 UTC 175.35.84.12

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET dogs with their size and owner's username. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
