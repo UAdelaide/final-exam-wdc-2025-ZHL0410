aad91e121dff6211b46e9b6ffc7222332c4d5558
ZHL0410 <a1920989@adelaide.edu.au>
2025-06-21 00:47:17 +0000

    [automatic commit] 2025-06-21 00:47:17 UTC 175.35.84.12

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -5,10 +5,10 @@ const db = require('../models/db');
 // GET all dogs
 router.get('/', async (req, res) => {
   try {
-    const [rows] = await db.query('SELECT dog_id, name, size, role FROM Users');
+    const [rows] = await db.query('SELECT dog_id, name, size, role FROM Dogs');
     res.json(rows);
   } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch users' });
+    res.status(500).json({ error: 'Failed to fetch dogs' });
   }
 });
 
