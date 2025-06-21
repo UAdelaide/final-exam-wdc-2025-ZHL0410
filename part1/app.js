c1cac24af54369f3154c2280b963e7305f0b6ac4
ZHL0410 <a1920989@adelaide.edu.au>
2025-06-21 05:01:53 +0000

    [automatic commit] 2025-06-21 05:01:53 UTC 175.35.84.12

--- part1/app.js
+++ part1/app.js
@@ -90,7 +90,21 @@ let db;
     `);
 
       // Create a WalkRatings table if it doesn't exist
-      await db.query(``);
+      await db.query(`
+        CREATE TABLE WalkRatings (
+          rating_id INT AUTO_INCREMENT PRIMARY KEY,
+          request_id INT NOT NULL,
+          walker_id INT NOT NULL,
+          owner_id INT NOT NULL,
+          rating INT CHECK (rating BETWEEN 1 AND 5),
+          comments TEXT,
+          rated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+          FOREIGN KEY (request_id) REFERENCES WalkRequests(request_id),
+          FOREIGN KEY (walker_id) REFERENCES Users(user_id),
+          FOREIGN KEY (owner_id) REFERENCES Users(user_id),
+          CONSTRAINT unique_rating_per_walk UNIQUE (request_id)
+      )
+      `);
 
     // Insert data if Users table is empty
     const [rowsOfUsers] = await db.execute('SELECT COUNT(*) AS count FROM Users');
