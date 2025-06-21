0436f1b739c414827e3694741e8065d0d16dbb73
ZHL0410 <a1920989@adelaide.edu.au>
2025-06-21 00:45:14 +0000

    [automatic commit] 2025-06-21 00:45:14 UTC 175.35.84.12

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
 
-// GET all users (for admin/testing)
+// GET all dogs
 router.get('/', async (req, res) => {
   try {
     const [rows] = await db.query('SELECT user_id, username, email, role FROM Users');
